f522598cf30937a8ce7ef798a3a67ac799da2369
monica nguyen-vo <monnvart@gmail.com>
2020-07-20 02:52:15 -0500

    setting up choices and game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic
+
+public class GameManager : MonoBehaviour
+{
+
+    public Questions[] questions;
+    private static List<Questions> unansweredQuestions;
+
+    private void Start()
+    {
+        if (unansweredQuestions == null)
+        {
+            unansweredQuestions = questions;
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/Questions.cs
@@ -0,0 +1,11 @@
+﻿
+[System.Serializable]
+public class Questions
+{
+
+    public string questions;
+    public bool isTrue;
+    public bool isFalse;
+
+
+}
